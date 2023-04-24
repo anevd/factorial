fc47bf34375c5f05fd868ce18e793aa5efe1b02b
mkarsums15@mail.ru <mkarsums15@mail.ru>
2023-04-24 22:31:57 +0300

    add task

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 // Итерационный подсчет факториала
-function factIterat(num){
-
+function factIterat(num) {
+	let result = 1;
+	for (let i = 1; i <= num; i++) {
+		result *= i;
+	}
+	return result;
 }
 
 // Рекурсивный расчет факториала
-function factRec(num){
- 
+function factRec(num) {
+	if (num === 1) return 1;
+	return num * factRec(num - 1);
 }
