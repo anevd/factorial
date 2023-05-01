50f173172ed77742c15cc4c088aa9a6c96908108
mkarsums15@mail.ru <mkarsums15@mail.ru>
2023-05-01 21:06:02 +0300

    add solution for incorrect values

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 // Итерационный подсчет факториала
 function factIterat(num) {
-	let result = 1;
-	for (let i = 1; i <= num; i++) {
-		result *= i;
+	if (num <= 0) return "Некорректное значение для вычисления факториала";
+	else {
+		let result = 1;
+		for (let i = 1; i <= num; i++) {
+			result *= i;
+		}
+		return result;
 	}
-	return result;
 }
 
 // Рекурсивный расчет факториала
 function factRec(num) {
+	if (num <= 0) return "Некорректное значение для вычисления факториала";
 	if (num === 1) return 1;
 	return num * factRec(num - 1);
 }
